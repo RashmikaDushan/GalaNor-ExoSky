14c44cf28d58745016ea2d4b4a1437391bd303f2
RashmikaDushan <kbrashmikadushan@gmail.com>
2024-10-06 09:18:44 +0530

    cube rendering added

--- frontend/src/pages/SkyView.jsx
+++ frontend/src/pages/SkyView.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
 
 function App() {
   const mountRef = useRef(null);
@@ -7,25 +10,69 @@ function App() {
   useEffect(() => {
     // Scene
     const scene = new THREE.Scene();
-
-    // Camera (PerspectiveCamera)
     const camera = new THREE.PerspectiveCamera(
-      75, // Field of View
-      window.innerWidth / window.innerHeight, // Aspect Ratio
-      0.1, // Near clipping plane
-      1000 // Far clipping plane
+      75,
+      window.innerWidth / window.innerHeight,
+      1,
+      10000
     );
+    // camera----------------------------------------------------------------
     camera.position.z = 5;
 
     // Renderer
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
 
+    // Set up Unreal Bloom Pass----------------------------------------------
+    const renderScene = new RenderPass(scene, camera);
+    const bloomPass = new UnrealBloomPass(
+      new THREE.Vector2(window.innerWidth, window.innerHeight),
+      2.0, // Strength of the bloom effect
+      1.0, // Bloom radius
+      0.2 // Bloom threshold
+    );
+
+    const composer = new EffectComposer(renderer);
+    composer.addPass(renderScene);
+    composer.addPass(bloomPass);
+
+    const geometry = new THREE.BufferGeometry();
+
+    // create a simple square shape. We duplicate the top left and bottom right
+    // vertices because each vertex needs to appear once per triangle.
+    const vertices = new Float32Array([
+      -1.0,
+      -1.0,
+      1.0, // v0
+      1.0,
+      -1.0,
+      1.0, // v1
+      1.0,
+      1.0,
+      1.0, // v2
+
+      1.0,
+      1.0,
+      1.0, // v3
+      -1.0,
+      1.0,
+      1.0, // v4
+      -1.0,
+      -1.0,
+      1.0, // v5
+    ]);
+
+    // itemSize = 3 because there are 3 values (components) per vertex
+    geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
     // Cube (Geometry + Material)
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshStandardMaterial({ color: "orange" });
-    const cube = new THREE.Mesh(geometry, material);
+    const cubegeometry = new THREE.BoxGeometry();
+    const cubematerial = new THREE.MeshStandardMaterial({ color: "orange" });
+    const cube = new THREE.Mesh(cubegeometry, cubematerial);
     scene.add(cube);
 
     // Light
