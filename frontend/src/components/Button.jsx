741c695a7c20c1d9269fd6db615dc93c67a4b90a
Theekshana udara <theekshana.uda@gmail.com>
2024-10-06 22:33:18 +0530

    Refactor Button component to navigate to Sky View page

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,10 +1,12 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../style/Button.css'; // Assuming there is a CSS file for styling the button
 
-const Button = ({ label, to, external,  ...props }) => {
-  // If `external` is true, we use an anchor (`<a>`) tag for external links
+const Button = ({ label, to, external, ...props }) => {
+  const goToGoogle = () => {
+    window.location.href = "/exoskyview";
+  };
+
   if (external) {
     return (
       <a href={to} {...props} target="_blank" rel="noopener noreferrer">
@@ -13,25 +15,10 @@ const Button = ({ label, to, external,  ...props }) => {
     );
   }
 
-  // Otherwise, we assume it's an internal link and use `Link` from `react-router-dom`
   return (
     <Link to={to} className={`button`} {...props}>
       {label}
     </Link>
-=======
-import React from "react";
-import "../style/Button.css"; // Ensure to have Button styles here
-
-const Button = () => {
-  const goToGoogle = () => {
-    window.location.href = "/exoskyview";
-  };
-
-  return (
-    <button onClick={goToGoogle} className="button">
-      🚀 Explore Google
-    </button>
->>>>>>> origin/earthview
   );
 };
 
